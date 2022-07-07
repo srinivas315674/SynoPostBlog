2b72b46bf67f74c7a756aa8989e0f16a8718d4ec
srinivas315674 <ramireddy.srinivasa@synoriq.in>
2022-07-07 18:40:18 +0530

    Initial Commit

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Blog({ url }) {
   return (
-    <div className="d-inline-flex p-2" >
+    <div className="d-inline-flex p-2  " >
       <iframe src={url}scrolling='yes' className="iframes"> </iframe>
     </div>
   );
